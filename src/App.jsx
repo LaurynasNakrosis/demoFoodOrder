764ba74d044424fa782645f5bf0df5ca5586f5fa
k1234906@kingston.ac.uk <k1234906@kingston.ac.uk>
2026-02-05 15:39:13 +0000

    Add Meals component to fetch and display meal data in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // © 2026 Lawrence Nakrosis. All rights reserved.
 import Header from './components/Header'
+import Meals from './components/Meals';
 function App() {
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }
