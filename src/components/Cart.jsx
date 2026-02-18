dbca63b7589b125ceb4d692e1b09d28b697ddb9a
k1234906@kingston.ac.uk <k1234906@kingston.ac.uk>
2026-02-18 16:08:47 +0000

    Enhance Modal component to support onClose prop; update Cart and Checkout components to utilize new functionality for improved user experience and modal management.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -23,7 +23,7 @@ export default function Cart() {
     }
 
     return (
-        <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+        <Modal className="cart" onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null} open={userProgressCtx.progress === 'cart'}>
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map((item) => (
